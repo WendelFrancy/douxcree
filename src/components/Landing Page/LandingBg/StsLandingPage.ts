348a3b7611338c33eaf950a77f898068ff54b517
WendelF <wendelfas@hotmail.com>
2024-12-17 16:38:13 -0300

    New Assets

--- src/components/Landing Page/LandingBg/StsLandingPage.ts
+++ src/components/Landing Page/LandingBg/StsLandingPage.ts
@@ -39,7 +39,7 @@ export const DouxTxtContainer = styled.div`
 
 export const DouxCree = styled.h1`
     font-family: "Playfair Display", serif;
-    font-size: 15rem;
+    font-size: 17rem;
     font-weight: 600;
     color: #FFEFEF;
         strong {
