0cd759df0821c455bcdc92fa5b176cb0793abce6
WFran <wendelfas@hotmail.com>
2025-05-14 21:42:54 -0300

    Testing Container on Section

--- src/components/home/calltoaction/CallToAction.tsx
+++ src/components/home/calltoaction/CallToAction.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import './styles/base.scss';
+import React from "react";
+import "./styles/main.scss";
 
 const CallToAction: React.FC = () => {
-    return(
+  return (
     <>
       <section>
-        <h1>Beleza Francesa para Mulheres Sofisticadas</h1>
-        <div>
-            <button> Explorar Produtos 
-                <img src="#" alt="Seta para Direita" />
-            </button>
-            <button> Nossa História </button>
+        <div id="secContainer">
+          <h1>Beleza Francesa para Mulheres Sofisticadas</h1>
+          <p>
+            Descubra o segredo da beleza francesa com nossa linha exclusiva de
+            cosméticos de luxo, formulados com ingredientes naturais da França.
+          </p>
         </div>
-
+        <div>
+          <button>
+            {" "}
+            Explorar Produtos
+            <img
+              src="src\assets\arrow-pointing-right_1131.png"
+              alt="Seta para direita"
+            />
+          </button>
+          <button> Nossa História </button>
+        </div>
       </section>
-      
     </>
-    )
-}
+  );
+};
 
-export default CallToAction
+export default CallToAction;
