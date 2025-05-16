6fc575f5d8598264a02be3754fad0baaadd75982
WFran <wendelfas@hotmail.com>
2025-05-16 14:04:49 -0300

    Btn Design

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "./menu/Menu";
 import CallToAction from "./calltoaction/CallToAction";
+import "./homeStyles/main.scss";
 
 const Home: React.FC = () => {
   return (
--- src/components/home/calltoaction/CallToAction.tsx
+++ src/components/home/calltoaction/CallToAction.tsx
@@ -6,22 +6,21 @@ const CallToAction: React.FC = () => {
     <>
       <section>
         <div id="secContainer">
-          <h1>Beleza Francesa para Mulheres Sofisticadas</h1>
+          <h1><span>Beleza Francesa</span>para Mulheres Sofisticadas</h1>
           <p>
             Descubra o segredo da beleza francesa com nossa linha exclusiva de
             cosméticos de luxo, formulados com ingredientes naturais da França.
           </p>
         </div>
         <div>
-          <button>
-            {" "}
+          <button className="btnExplore">
             Explorar Produtos
             <img
               src="src\assets\arrow-pointing-right_1131.png"
               alt="Seta para direita"
             />
           </button>
-          <button> Nossa História </button>
+          <button className="btnHistory"> Nossa História </button>
         </div>
       </section>
     </>
