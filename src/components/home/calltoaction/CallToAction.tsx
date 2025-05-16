a941978e09654eebcfe1ad637845ba710a3debda
WFran <wendelfas@hotmail.com>
2025-05-16 16:07:32 -0300

    Finishing Call To Action Section

--- src/components/home/calltoaction/CallToAction.tsx
+++ src/components/home/calltoaction/CallToAction.tsx
@@ -5,23 +5,29 @@ const CallToAction: React.FC = () => {
   return (
     <>
       <section>
-        <div id="secContainer">
-          <h1><span>Beleza Francesa</span>para Mulheres Sofisticadas</h1>
-          <p>
-            Descubra o segredo da beleza francesa com nossa linha exclusiva de
-            cosméticos de luxo, formulados com ingredientes naturais da França.
-          </p>
+        <div id="homeContainer">
+          <div id="homeTextContainer">
+            <h1>
+              <span>Beleza Francesa</span> para Mulheres Sofisticadas
+            </h1>
+            <p>
+              Descubra o segredo da beleza francesa com nossa linha exclusiva de <br/>
+              cosméticos de luxo, formulados com ingredientes naturais da
+              França.
+            </p>
+          </div>
+          <div>
+            <button className="btnExplore">
+              Explorar Produtos
+              <img
+                src="src\assets\arrow-pointing-right_1131.png"
+                alt="Seta para direita"
+              />
+            </button>
+            <button className="btnHistory"> Nossa História </button>
+          </div>
         </div>
-        <div>
-          <button className="btnExplore">
-            Explorar Produtos
-            <img
-              src="src\assets\arrow-pointing-right_1131.png"
-              alt="Seta para direita"
-            />
-          </button>
-          <button className="btnHistory"> Nossa História </button>
-        </div>
+        <img id="homeImage" src="src\assets\home-img.jpg" alt="Home Image" />
       </section>
     </>
   );
