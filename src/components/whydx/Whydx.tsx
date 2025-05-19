fb9503d15f76bc5187545be7c270e94c2f1fa748
WFran <wendelfas@hotmail.com>
2025-05-19 15:05:18 -0300

    Adding WhyDx section

--- src/components/whydx/Whydx.tsx
+++ src/components/whydx/Whydx.tsx
@@ -5,25 +5,27 @@ const Whydx: React.FC = () => {
     return(
     <>
         <section id="WDXSection">
-            <div id="WDXtxtContainer">
-                <h1>Por que escolher <span>DOUXCREÉ</span></h1>
-                <p>Nossa marca combina a tradição francesa de beleza com ingredientes premium e tecnologia avançada.</p>
-            </div>
-            <div id="WDXCardContainer">
-                <div className="cardContainer">
-                    <img className="cardImg" src="" alt="" />
-                    <h3> Ingedientes Naturais </h3>
-                    <p> Utilizamos apenas ingredientes naturais selecionados das melhores regiões da França. </p>
+            <div id="WDXContainer">
+                <div id="WDXtxtContainer">
+                    <h1>Por que escolher <span>DOUXCREÉ</span></h1>
+                    <p>Nossa marca combina a tradição francesa de beleza com ingredientes premium e tecnologia avançada.</p>
                 </div>
-                <div className="cardContainer">
-                    <img className="cardImg" src="" alt="" />
-                    <h3> Fórmulas Exclusivas </h3>
-                    <p> Nossas fórmulas são desenvolvidas por especialistas em cosmetologia francesa.</p>
-                </div>
-                <div className="cardContainer">
-                    <img className="cardImg" src="" alt="" />
-                    <h3> Sustentabilidade </h3>
-                    <p> Comprometidos com embalagens recicláveis e processos de produção sustentáveis.</p>
+                <div id="WDXCardContainer">
+                    <div className="cardContainer">
+                        <img className="cardImg" src="src\assets\obsolete-assets\lolalogo.png" alt="" />
+                        <h3> Ingredientes Naturais </h3>
+                        <p> Utilizamos apenas ingredientes naturais selecionados das melhores regiões da França. </p>
+                    </div>
+                    <div className="cardContainer">
+                        <img className="cardImg" src="src\assets\obsolete-assets\lolalogo.png" alt="" />
+                        <h3> Fórmulas Exclusivas </h3>
+                        <p> Nossas fórmulas são desenvolvidas por especialistas em cosmetologia francesa.</p>
+                    </div>
+                    <div className="cardContainer">
+                        <img className="cardImg" src="src\assets\obsolete-assets\lolalogo.png" alt="" />
+                        <h3> Sustentabilidade </h3>
+                        <p> Comprometidos com embalagens recicláveis e processos de produção sustentáveis.</p>
+                    </div>
                 </div>
             </div>
         </section>
